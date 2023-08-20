e9bafc0d39804ab7dcea3182144491b548d8f0c4
Constantin <constantinmeier@gmx.de>
2023-08-20 20:15:16 +0200

    edit - removed all unnessessary stuff

--- src/main.cpp
+++ src/main.cpp
@@ -14,74 +14,8 @@
 #include <servo.h>
 #include <ArduinoEigenDense.h>
 
-#include <Wire.h>
-#include <VL53L0X.h>
-
-//initalize VL53L0X sensor
-VL53L0X tofSensor;
-
 Servo servo_a;
 Servo servo_b;
-
-#define VRX_PIN 34 // ESP32 pin GIOP36 (ADC0) connected to VRX pin
-#define VRY_PIN 35 // ESP32 pin GIOP39 (ADC0) connected to VRY pin
-
-#define CONTROL_PIN 33 //Pin to activate the servos
-
-int neutralX = 0; // to store the X-axis neutral position
-int neutralY = 0; // to store the Y-axis neutral position
-int minX = 4095;  // min X position during calibration
-int maxX = 0;     // max X position during calibration
-int minY = 4095;  // min Y position during calibration
-int maxY = 0;     // max Y position during calibration
-
-struct Point {
-    double x;
-    double y;
-    double mappedX;
-    double mappedY;
-};
-
-void calibrateJoystick() {
-  Serial.println("Please keep the joystick still for 5 seconds");
-
-  unsigned long startTime = millis();
-  long sumX = 0, sumY = 0;
-  int count = 0;
-
-  while (millis() - startTime < 5000) {
-    sumX += analogRead(VRX_PIN);
-    sumY += analogRead(VRY_PIN);
-    count++;
-    delay(10);
-  }
-
-  neutralX = sumX / count;
-  neutralY = sumY / count;
-
-  Serial.println("Please move the joystick around for 5 seconds");
-
-  minX = 4095;  // min X position during calibration
-  maxX = 0;     // max X position during calibration
-  minY = 4095;  // min Y position during calibration
-  maxY = 0;     // max Y position during calibration
-
-  startTime = millis();
-  while (millis() - startTime < 5000) {
-    int valueX = analogRead(VRX_PIN);
-    int valueY = analogRead(VRY_PIN);
-    minX = min(minX, valueX);
-    maxX = max(maxX, valueX);
-    minY = min(minY, valueY);
-    maxY = max(maxY, valueY);
-    delay(10);
-  }
-}
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-//Angles calculated using parameters and alpha beta func
 
 // Adjusted parameters for alpha function
 Eigen::Vector3f a_0(-23, 0, 45);
@@ -142,104 +76,30 @@ float beta(float psi, float phi, const Eigen::Matrix3f& R) {
     return 2 * atan((x[1] + pm * sqrt(sqrt_val)) / denominator) * 180.0 / M_PI; // convert result from radians to degrees
 }
 
+// Global offset variables
+float offset_alpha = -25; // Adjust this value based on your needs
+float offset_beta = -35;  // Adjust this value based on your needs
+
 
 void setup() {
     Serial.begin(9600);
-    while (!Serial); // Warten Sie, bis die serielle Verbindung hergestellt ist
-
-    Wire.begin(); 
-    delay(10);
-    pinMode(CONTROL_PIN, INPUT); // Setzen Sie den Pin als Eingang
-    delay(1500);
+    delay(1000);  // Give a delay for the serial connection to establish
+    Serial.println("Starting system setup...");
     
-    if (!tofSensor.init()) {
-      Serial.println("Failed to initialize the TOF200C sensor!");
-      while (1); // Endlos-Schleife, um das Programm anzuhalten
-    } else {
-      Serial.println("TOF200C sensor initialized successfully!");
-    }
-    tofSensor.setTimeout(500); // Setze Timeout auf 500ms
-    tofSensor.startContinuous(50); // start continuous mode with 20ms delay between measurements
-
-
-    if (false) { // Überprüfen Sie, ob der Pin HIGH ist
-      servo_a.attach(12);
-      servo_b.attach(13);
-      delay(1000);
-      servo_a.write(0); // Setzen Sie die Servos auf 0
-      servo_b.write(0);
-      Serial.println("System stopped by control pin");
-      delay(10000);
-      setup(); // Starten Sie das Setup erneut
-    }
-
-    calibrateJoystick();
-
     servo_a.attach(12);
     servo_b.attach(13);
     delay(1000);
-    Serial.println("System started");
+    Serial.println("Servos attached.");
 
-    //Serial print Alpha and Beta angles for testing
+    // Initial angle setting
     Eigen::Matrix3f R = calculate_R(0, 0);
-    Serial.println(alpha(0, 0, R));
-    Serial.println(beta(0, 0, R));
+    Serial.println("Setting initial angles...");
+    servo_a.write(alpha(0, 0, R) + offset_alpha);
+    servo_b.write(160 + beta(0, 0, R) + offset_beta);
+    Serial.println("Initial angles set. Waiting for Serial..");
 
-    servo_a.write(alpha(0, 0, R));
-    servo_b.write(180 + beta(0, 0, R));
-} 
+}
 
 void loop() {
-  
-  //offset controlls
-  float offset_a = -12;
-  float offset_b = -12;
-
-  // read X and Y analog values
-  int valueX = analogRead(VRX_PIN);
-  int valueY = analogRead(VRY_PIN);
-
-  // Messen Sie die Entfernung:
-  uint16_t distance = tofSensor.readRangeContinuousMillimeters();
-  if (tofSensor.timeoutOccurred()) {
-    Serial.println("Sensor timeout!");
-  }
-
-
-  // calibrate the values
-  float calibratedX, calibratedY;
-
-  if (valueX > neutralX) {
-    calibratedX = mapFloat(valueX, neutralX, maxX, 0, 7.5);
-  } else {
-    calibratedX = mapFloat(valueX, minX, neutralX, -7.5, 0);
-  }
-
-  if (valueY > neutralY) {
-    calibratedY = mapFloat(valueY, neutralY, maxY, 0, 7.5);
-  } else {
-    calibratedY = mapFloat(valueY, minY, neutralY, -7.5, 0);
-  }
-
-  // calculate rotation matrix
-  Eigen::Matrix3f R = calculate_R(calibratedX, calibratedY);
-  float alpha_value = alpha(calibratedX, calibratedY, R);
-  float beta_value = beta(calibratedX, calibratedY, R);
-  servo_a.write(alpha_value - offset_a);
-  servo_b.write(135 + beta_value - offset_b);
-
-  // print values
-  Serial.print("Joystick value X:");
-  Serial.print(calibratedX, 2);  // print with 2 decimal places
-  Serial.print(", Y: ");
-  Serial.print(calibratedY, 2);  // print with 2 decimal places
-  Serial.print(", Alpha:");
-  Serial.print(alpha_value, 2);  // print with 2 decimal places
-  Serial.print(", Beta:");
-  Serial.print(-1 * beta_value, 2);  // print with 2 decimal places
-  Serial.print(", Distance: ");
-  Serial.print(tofSensor.readRangeContinuousMillimeters());
-  Serial.println("mm");
-
-  delay(10);
+    // Nothing to do here
 }
